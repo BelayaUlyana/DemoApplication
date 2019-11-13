8a6432479d97727d3cde7f07ac5eacf615552d25
belaya.ulyana <belaya.ulyana@gmail.com>
2019-11-13 23:26:33 +0200

    Initial commit

---
+++ app/src/main/java/com/mycompany/testtask/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mycompany.testtask;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.background_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, UsersScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3 * 1000);
+    }
+}
---
+++ app/src/main/java/com/mycompany/testtask/UsersScreen.java
@@ -0,0 +1,13 @@
+package com.mycompany.testtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsersScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_screen);
+    }
+}
